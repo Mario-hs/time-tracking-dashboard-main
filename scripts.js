7177a67f3c2a400c3eaa9b077aac6f09e583f758
Mario Henrique <mario-hp41@hotmail.com>
2021-10-15 11:27:24 -0300

    comentários

--- scripts.js
+++ scripts.js
@@ -1,6 +1,9 @@
 // Função para ler os dados do arquivo data.json e distribuir entre dia, semana e mês
 async function getData(value) {
+    // async é usado no começo da função quando é usado await
     try {
+        // fetch promete busca o arquivo através da URL  
+        // await para esperar a resposta do fetch
         let request = await fetch('./data.json')
         let data = await request.json()
 
@@ -27,19 +30,27 @@ async function getData(value) {
 function showData(data, time) {
     let output = ''
     if (time === 'daily') {
+        // o primeiro document.querySelctor é para zera as informações que possam estar no bloco
         document.querySelector(`.${data.title}`).querySelector('.info').innerHTML = output
+
         output += `<p>${data.timeframes.daily.current}hrs</p>
                    <span>Last Week - ${data.timeframes.daily.previous}hrs</span>`
+
+        // recebe e insere os valores no html
         document.querySelector(`.${data.title}`).querySelector('.info').innerHTML += output
     } else if (time === 'monthly') {
         document.querySelector(`.${data.title}`).querySelector('.info').innerHTML = output
+
         output += `<p>${data.timeframes.monthly.current}hrs</p>
                     <span>Last Week - ${data.timeframes.monthly.previous}hrs</span>`
+
         document.querySelector(`.${data.title}`).querySelector('.info').innerHTML += output
     } else {
         document.querySelector(`.${data.title}`).querySelector('.info').innerHTML = output
+
         output += `<p>${data.timeframes.weekly.current}hrs</p>
                 <span>Last Week - ${data.timeframes.weekly.previous}hrs</span>`
+
         document.querySelector(`.${data.title}`).querySelector('.info').innerHTML += output
     }
 }
@@ -47,17 +58,23 @@ function showData(data, time) {
 // Função para trocar o botão de destaque colocando a classe active
 function handleActiveButton(time) {
     if (time === 'daily') {
+        // adiciona a classe no botão que foi clicado e remove a classe dos outros botões
         document.querySelector('#daily').classList.add('active')
         document.querySelector('#weekly').classList.remove('active')
         document.querySelector('#monthly').classList.remove('active')
+
     } else if (time === 'monthly') {
+
         document.querySelector('#monthly').classList.add('active')
         document.querySelector('#daily').classList.remove('active')
         document.querySelector('#weekly').classList.remove('active')
+
     } else {
+
         document.querySelector('#weekly').classList.add('active')
         document.querySelector('#daily').classList.remove('active')
         document.querySelector('#monthly').classList.remove('active')
+
     }
 }
 
