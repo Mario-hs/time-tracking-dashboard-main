c193596ef469c6b1d15fb8047fec37c41a4e00d3
Mario Henrique <mario-hp41@hotmail.com>
2021-10-15 11:13:09 -0300

    script de leitura do arquivo .json e consumo dele

--- scripts.js
+++ scripts.js
@@ -5,32 +5,57 @@ async function getData(value) {
 
         data.map((props) => {
             let time = value
-            console.log('>', props)
-            if (time == 1) {
-                console.log('>>', props.timeframes.daily)
+            if (time === 1) {
                 handleActiveButton('daily')
-            } else if (time == 2) {
-                console.log('>>', props.timeframes.weekly)
+                showData(props, 'daily')
+            } else if (time === 2) {
                 handleActiveButton('weekly')
+                showData(props, 'weekly')
             } else {
-                console.log('>>', props.timeframes.monthly)
                 handleActiveButton('monthly')
+                showData(props, 'monthly')
             }
         })
+
     } catch (error) {
         console.error(error)
     }
 }
 
-function handleActiveButton(time) {
-    // document.querySelector('#daily').classList.remove('active')
-    // document.querySelector('#weekly').classList.remove('active')
-    // document.querySelector('#monthly').classList.remove('active')
-
-    if (document.querySelector(`#${time}`) != time) {
-        document.querySelector(`#${time}`).classList.add('active')
+function showData(data, time) {
+    let output = ''
+    if (time === 'daily') {
+        document.querySelector(`.${data.title}`).querySelector('.info').innerHTML = output
+        output += `<p>${data.timeframes.daily.current}hrs</p>
+                   <span>Last Week - ${data.timeframes.daily.previous}hrs</span>`
+        document.querySelector(`.${data.title}`).querySelector('.info').innerHTML += output
+    } else if (time === 'monthly') {
+        document.querySelector(`.${data.title}`).querySelector('.info').innerHTML = output
+        output += `<p>${data.timeframes.monthly.current}hrs</p>
+                    <span>Last Week - ${data.timeframes.monthly.previous}hrs</span>`
+        document.querySelector(`.${data.title}`).querySelector('.info').innerHTML += output
+    } else {
+        document.querySelector(`.${data.title}`).querySelector('.info').innerHTML = output
+        output += `<p>${data.timeframes.weekly.current}hrs</p>
+                <span>Last Week - ${data.timeframes.weekly.previous}hrs</span>`
+        document.querySelector(`.${data.title}`).querySelector('.info').innerHTML += output
     }
 }
 
+function handleActiveButton(time) {
+    if (time === 'daily') {
+        document.querySelector('#daily').classList.add('active')
+        document.querySelector('#weekly').classList.remove('active')
+        document.querySelector('#monthly').classList.remove('active')
+    } else if (time === 'monthly') {
+        document.querySelector('#monthly').classList.add('active')
+        document.querySelector('#daily').classList.remove('active')
+        document.querySelector('#weekly').classList.remove('active')
+    } else {
+        document.querySelector('#weekly').classList.add('active')
+        document.querySelector('#daily').classList.remove('active')
+        document.querySelector('#monthly').classList.remove('active')
+    }
+}
 
 getData(2)
